f0708e4b386d59727ca7654e45a09674fd00efc3
MkMyApp <mshinoda@big.or.jp>
2026-03-19 16:31:28 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,47 @@
+﻿const CACHE_NAME = "text-editor-" + Date.now();
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./file.js",
+  "./manifest.json",
+  "./sw-register.js",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    Promise.all([
+      caches.keys().then(keys => {
+        return Promise.all(
+          keys.map(key => {
+            if (key !== CACHE_NAME) {
+              return caches.delete(key);
+            }
+          })
+        );
+      }),
+      self.clients.claim()
+    ])
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
